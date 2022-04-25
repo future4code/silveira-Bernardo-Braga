c7099f1cf7dadb59ce5f0747545daebc2d8a2206
berbraga <bernardo.sbraga@yahoo.com>
2022-04-25 17:07:02 -0300

    projeto LabeX

---
+++ modulo3/labe-x/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+// import axios from 'axios';
+
+const MainContainer = styled.div`
+
+`
+
+function App() {
+  return (
+    <MainContainer>
+    </MainContainer>
+  );
+}
+
+export default App;
