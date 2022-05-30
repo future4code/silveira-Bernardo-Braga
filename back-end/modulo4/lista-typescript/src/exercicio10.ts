29738351680e6f8aaa285cb8d3b1af128ce93569
berbraga <bernardo.sbraga@yahoo.com>
2022-05-30 09:05:40 -0300

    bernardo

--- back-end/modulo4/lista-typescript/src/exercicio10.ts
+++ back-end/modulo4/lista-typescript/src/exercicio10.ts
@@ -1,6 +1,73 @@
 
 
 
+// O CPF consiste de 11 dígitos cuja configuração respeita o formato XXX.XXX.XXX-XX.
+
+const ELEVEN = 11
+
+function noSpecial(cpf:string):string {
+    let cpfSemEspecial = ''
+    for (let i = 0; i < cpf.length  ; i++) {
+        
+        if (i != 3 && i != 7 && i != 11 ){
+            cpfSemEspecial += cpf[i]
+        }
+    
+    }
+    return cpfSemEspecial
+}
+
+function verificaDV(str :string, dvNumber:number):boolean {
+    let index = dvNumber + 1
+    let soma = 0
+
+    for (let i = dvNumber; i >= 0 ; i--) {
+        soma += index * Number(str[i])
+        index--
+    }
+
+    const resto = soma % (dvNumber + 1)
+
+    let calcDV = ELEVEN - resto
+    
+    if (calcDV >= 10) {
+        calcDV = 0
+
+    }
+
+    if (calcDV === Number(str[dvNumber])) {
+        return true
+    }
+
+
+    return false
+}
+
+
+function validaCPF (str:string) :boolean{
+    let cpf = noSpecial(str)
+
+    let dv1 = verificaDV(cpf,9)
+    let dv2 = verificaDV(cpf,10)
+
+    if (dv1 == dv2) {
+        return true
+    }
+
+    return false
+}
+
+
+
+const cpf = '006'
+console.log(validaCPF(cpf));
+
+
+
+
+
+
+
 
 
 
--- back-end/modulo4/lista-typescript/src/exercicio8.ts
+++ back-end/modulo4/lista-typescript/src/exercicio8.ts
@@ -1,5 +1,56 @@
     let dataNascimento:string =  process.argv[2] 
     let emissaoCarteira:string =  process.argv[3] 
-
+    let dataAtual = new Date()
     
+
+    function getYear (data:string) :number {
+        let year :string ='' 
+        for ( let i = 6 ; i < data.length  ; i++ ) {
+            year += data[i]
+        }
+        return Number(year)
+    }
+
+    function renovada(  ) :boolean {
+        const anoAtual = dataAtual.getFullYear()
+        
+        const anoNascimento = getYear(dataNascimento)
+        const anoEmissao = getYear(emissaoCarteira)
+        
+        const idade = anoAtual - anoNascimento
+        const idadeEmissao = anoAtual - anoEmissao
+
+
+        
+
+        if ( idade <= 20 ) {
+            if (idadeEmissao >= 5 ) {
+                return true
+            }
+            return false
+
+            
+        }
+        if ( idade > 20 && idade <= 50 ) {
+            if (idadeEmissao >= 10 ) {
+                return true
+            }
+            return false
+        }
+        if ( idade > 50 ) {
+            if (idadeEmissao >= 15 ){
+                return true
+            }
+            return false
+        }
+
+       return false
+
+    }
+
+
+
+
+
+    console.log(renovada() )//typeof(dataAtual.getFullYear())
 
