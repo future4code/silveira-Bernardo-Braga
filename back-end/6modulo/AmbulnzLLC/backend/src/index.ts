ef14f93bf6082e12caf06350b8e00f08f76210af
berbraga <bernardo.sbraga@yahoo.com>
2022-08-08 16:32:58 -0300

    adicionando um comentario de exemplo

--- back-end/6modulo/AmbulnzLLC/backend/src/index.ts
+++ back-end/6modulo/AmbulnzLLC/backend/src/index.ts
@@ -2,4 +2,58 @@ import dotenv from "dotenv";
 import { app } from "./app";
 
 
+/* 
+
+EXEMPLO DE SAIDA ESPERADA 
+
+
+[
+  {
+    "name": "Margherita",
+    "price": 5,
+    "ingredients": [
+      "tomato",
+      "mozzarella"
+    ]
+  },
+  {
+    "name": "Bufala",
+    "price": 6,
+    "ingredients": [
+      "tomato",
+      "mozarella di bufala"
+    ]
+  },
+  {
+    "name": "Romana",
+    "price": 5,
+    "ingredients": [
+      "tomato",
+      "mozzarella",
+      "anchovies",
+      "oregano",
+      "oil"
+    ]
+  },
+  {
+    "name": "Diavola",
+    "price": 7.5,
+    "ingredients": [
+      "tomato",
+      "mozzarella",
+      "spicy salami"
+    ]
+  },
+  {
+    "name": "Pizza Bianca",
+    "price": 5,
+    "ingredients": [
+      "mozzarella",
+      "oregano"
+    ]
+  }
+]
+
+*/
+
 dotenv.config();
