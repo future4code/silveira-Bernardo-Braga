a7cdc7cbf363e8ae222d2ec654f71e21899d9c14
berbraga <bernardo.sbraga@yahoo.com>
2022-08-08 16:30:17 -0300

    projeto case4 iniciado com sucesso

--- back-end/6modulo/AmbulnzLLC/backend/DB.sql
+++ back-end/6modulo/AmbulnzLLC/backend/DB.sql
@@ -1,1 +1,20 @@
 -- Active: 1659980959740@@35.226.146.116@3306@silveira-21814729-bernardo-braga
+
+create table
+    AMBULNZLLC_PIZZA(
+        id varchar(255) PRIMARY KEY NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        price int not null DEFAULT 0
+    );
+
+create table
+    AMBULNZLLC_INGREDIENTS(
+        id varchar(255) PRIMARY KEY NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        id_pizza varchar(255),
+        CONSTRAINT fk_pizza_ingredientes FOREIGN KEY (id_pizza) REFERENCES AMBULNZLLC_PIZZA(id)
+    );
+
+select * from AMBULNZLLC_PIZZA ;
+
+select * from AMBULNZLLC_INGREDIENTS ;
